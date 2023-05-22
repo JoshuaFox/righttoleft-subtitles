0ff424f2fef2f8c7ac22082101edf23e2bcc4899
Joshua Fox <joshua@doit-intl.com>
2023-05-22 12:07:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,165 @@
+import os.path
+import sys
+import time
+import unicodedata
+from dataclasses import dataclass
+from functools import lru_cache
+from pathlib import Path
+from typing import ClassVar
+
+import ke
+
+timestamp_kex_str = '[2 #digit] ":" [2 #digit] ":" [2 #digit] "," [3 #digit]'
+
+
+@dataclass
+class Screen:
+    instances: ClassVar[int] = 0
+    timestamps: str = None
+    end_line_found: bool = False
+    txt: str = ""
+    original_sequence_number: int = -1
+    sequence_number: int = -1
+
+    def __post_init__(self):
+        Screen.instances += 1
+        self.sequence_number = Screen.instances
+
+    def __str__(self):
+        return f"""{self.sequence_number}
+{self.timestamps}
+{self.txt}\n"""
+
+
+@lru_cache
+def punc_ke():
+    s = "["
+    for c in ";:,.()„“?-—!":
+        s += '"' + c + '"|'
+    s += "#quote" + "|"
+    s += "#double_quote"  # no pipe at end here
+    #    s += "#left_brace" + "|" #Rendering of rtl text works correctly when this is NOT moved
+    #    s += "#right_brace" # should not occur EOL in rtl text, and if it does, we have worse problems
+    s += "]"
+    return s
+
+
+@lru_cache
+def rtl_ke():
+    s = "["
+    for c in range(ord("א"), ord("ת")):
+        s += '"' + chr(c) + '"|'
+    for c in range(
+            ord("\u0600"), ord("\u06FF")
+    ):  # Arabic range by unicode because it is not just arranged as alphabet
+        s += '"' + chr(c) + '"|'
+    s = s[:-1]
+    s += "]"
+
+    return s
+
+
+def normalize(lines):
+    ret = []
+    prev = "N/A"
+    lines = [l.strip() for l in lines]
+    for l in lines:
+        l=l.replace("\u2060","")#remove wordjoiner char
+        if prev != "" or l != "":
+            ret.append(l)# merge two or more blanklines
+        prev = l
+
+    return ret
+
+
+def move_punc(line):
+    assert line == line.strip(), "strip line first" + line
+    kexp = "[[1+ " + punc_ke() + "] #end_line]"
+
+    match = ke.search(kexp, line)
+    if match:
+        assert len(match.regs) == 1
+        start, end = match.regs[0]
+        assert end == len(line)  # regex finds punc at end of line
+        return line[start:] + line[:start]
+    else:
+        return line
+
+
+def is_nonneg_int(s):
+    return s.isdigit()
+
+
+def process(f_in, f_out):
+    screen = None
+    errors = []
+    with open(f_in) as f_i:
+        with open(f_out, "w") as file_out:
+            lines = f_i.readlines()
+            lines = normalize(lines)
+            prev_line = "N/A"
+            for line in lines:
+                screen = process_line(line, screen, prev_line, file_out, errors)
+                prev_line = line
+            if screen:  # if any lines at all
+                file_out.write(str(screen))
+            else:
+                raise ValueError("No screens found in file")
+    if errors:
+        print("Errors\n", "\n".join(errors))
+
+
+def process_line(line, current_screen, prev_line, file_out, errors):
+    if is_nonneg_int(line):
+        if current_screen:
+            if not current_screen.end_line_found:
+                errors.append(f"Blank-line missing:\n{current_screen}")
+            file_out.write(str(current_screen))
+        current_screen = Screen()
+        current_screen.original_sequence_number = int(line)
+    elif ke.match(
+            "[" + timestamp_kex_str + '" --> "' + timestamp_kex_str + "]", line
+    ):
+        if not current_screen: raise ValueError(f"Should reach a sequence number before timestamp {line}")
+        current_screen.timestamps = line
+    elif ke.search(rtl_ke(), line):
+
+        if not current_screen: raise ValueError(f"Should reach a sequence number before RTL text{line}")
+
+        line_rearranged = move_punc(line)
+        current_screen.txt += line_rearranged + "\n"
+    elif line == "":
+        if not current_screen: raise ValueError(f"Should reach a sequence number before blank line")
+        assert line != "\n" and prev_line != "", "Should not occur, because normalize() was used: Two end-lines found for screen " + current_screen
+        current_screen.end_line_found = True
+    else:
+        if not current_screen: raise ValueError(f"Should reach a sequence number before ordinary text {line}")
+        current_screen.txt += line + "\n"
+        if not is_ltr_sentence(line):
+            raise ValueError(f"expect LTR sentence, found {line}")
+    return current_screen
+
+
+def is_ltr_sentence(line):
+    sentence = ke.match(
+        "[#punc="
+        + punc_ke()
+        + " #start_line [1+ [#letter | #digit | #space| #punc]] #end_line]"
+        , line)
+    sentence_with_letter = ke.match("[1+[#letter ]]", line)
+    has_rtl = any((unicodedata.bidirectional(c) == "R") for c in line)
+    return sentence and sentence_with_letter and not has_rtl
+
+
+def main():
+    file_in = os.path.abspath(sys.argv[1])
+    path_in = Path(file_in)
+    file_out = Path(path_in.parent, "rtl_" + path_in.name)
+    print("output", file_out)
+    process(file_in, file_out)
+
+
+if __name__ == "__main__":
+    start = time.time()
+    main()
+    print(round(time.time() - start), "sec")
